563a632b8742149c110585e937f65b0290ad7ec3
Anish Balkhi <anishbalkhi1@gmail.com>
2026-09-19 13:53:32 +0530

    remove a line from originhero section

--- components/origin/OriginHero.tsx
+++ components/origin/OriginHero.tsx
@@ -122,7 +122,7 @@ export default function OriginHero({ onNavigate }: { onNavigate: (panelIndex: nu
               />
             </div>
 
-            <div className="h-px w-full bg-[#E8DCC8]/25 my-0.5" />
+            {/* <div className="h-px w-full bg-[#E8DCC8]/25 my-0.5" /> */}
 
             {/* Sidebar Navigation */}
             <ul className="flex flex-col border-b border-[#E8DCC8]/75">
